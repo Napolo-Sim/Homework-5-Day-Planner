a9c91e5ef59fb3d720c954074de3d43800a7f074
Napoleon Vuong <napolo101@gmail.com>
2020-05-02 15:09:14 -0700

    Displayed current Date in Header using moment in JS

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Linked?");
+$(document).ready(function () {
+    var currentDate = moment().format("MMMM Do YYYY");
+    console.log(currentDate);
+
+    var currentHour24 = moment().format("H");
+    console.log(currentHour24);
+
+    var currentHour12 = moment().format("h");
+    console.log(currentHour12);
+
+    var dateHeading = $("#currentDay");
+    dateHeading.text(currentDate);
+
+});
